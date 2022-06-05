c04027424bcd6c973d2595440ce1d1054b63e56f
Dominika <dominika.staniak4@gmail.com>
2022-06-05 17:23:37 +0200

    pierwsze podejscie

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import pymysql
+import csv
 
-if __name__ == "__main__":
+
+
+class ReportGenerator:
+  def __init__(self, host, port, user, passwd, database):
+    self.connection = pymysql.connect(host = "ego.kie.ue.poznan.pl", 
+                                      port = 8080,
+                                      user="inz_opr", 
+                                      passwd="inz_opr", 
+                                      database="inzynieria_oprogramowania")
+
+  def generate_report(self):
+    cursor = self.connection.cursor()
+    cursor.execute(f"Select sum(duration) from polaczenia")
+    result = cursor.fetchall()
+    print(result)
+    pass
+
+
+if __name__ == '__main__':
     pass
